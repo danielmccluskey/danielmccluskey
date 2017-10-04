63ab28939bc2fb85d9172fa6b72c7a910a90f840
danielmccluskey <dmccluskey98+Uni@gmail.com>
2017-10-04 21:52:52 +0100

    Changed image names

--- js/rand.js
+++ js/rand.js
@@ -1,22 +1,23 @@
 
 
 var imgsArray = [
-	["Georgopol", "https://pubg.gamepedia.com/Georgopol", "images/maps/Georgopol.png"],
-	["Mylta", "https://pubg.gamepedia.com/Mylta", "images/maps/Mylta.png"],
-	["Pochinki", "https://pubg.gamepedia.com/Pochinki", "images/maps/Pochinki.png"],
-	["Yasnaya Polyana", "https://pubg.gamepedia.com/Yasnaya_Polyana", "images/maps/Yasnaya.png"],
-	["Gatka", "https://pubg.gamepedia.com/Gatka", "images/maps/Gatka.png"],
-	["Kameshki", "https://pubg.gamepedia.com/Kameshki", "images/maps/Kameshki.png"],
-	["Lipovka", "https://pubg.gamepedia.com/Lipovka", "images/maps/Lipovka.png"],
-	["Mylta Power", "https://pubg.gamepedia.com/Mylta_Power", "images/maps/Mylta.png"],
-	["Novorepnoye", "https://pubg.gamepedia.com/Novorepnoye", "images/maps/Novorepnoye.png"],
-	["Primorsk", "https://pubg.gamepedia.com/Primorsk", "images/maps/Primorsk.png"],
-	["Rozhok", "https://pubg.gamepedia.com/Rozhok", "images/maps/Rozhok.png"],
-	["Severny", "https://pubg.gamepedia.com/Severny", "images/maps/Severny.png"],
-	["Stalber", "https://pubg.gamepedia.com/Stalber", "images/maps/Stalber.png"],
-	["Zharki", "https://pubg.gamepedia.com/Zharki", "images/maps/Zharki.png"],
-	["Sosnovka Military Base", "https://pubg.gamepedia.com/Sosnovka_Military_Base", "images/maps/Military.png"]
+	["Georgopol", "https://pubg.gamepedia.com/Georgopol", "images/maps/Georgopol.jpg"],
+	["Mylta", "https://pubg.gamepedia.com/Mylta", "images/maps/Mylta.jpg"],
+	["Pochinki", "https://pubg.gamepedia.com/Pochinki", "images/maps/Pochinki.jpg"],
+	["Yasnaya Polyana", "https://pubg.gamepedia.com/Yasnaya_Polyana", "images/maps/Yasnaya.jpg"],
+	["Gatka", "https://pubg.gamepedia.com/Gatka", "images/maps/Gatka.jpg"],
+	["Kameshki", "https://pubg.gamepedia.com/Kameshki", "images/maps/Kameshki.jpg"],
+	["Lipovka", "https://pubg.gamepedia.com/Lipovka", "images/maps/Lipovka.jpg"],
+	["Mylta Power", "https://pubg.gamepedia.com/Mylta_Power", "images/maps/Mylta.jpg"],
+	["Novorepnoye", "https://pubg.gamepedia.com/Novorepnoye", "images/maps/Novorepnoye.jpg"],
+	["Primorsk", "https://pubg.gamepedia.com/Primorsk", "images/maps/Primorsk.jpg"],
+	["Rozhok", "https://pubg.gamepedia.com/Rozhok", "images/maps/Rozhok.jpg"],
+	["Severny", "https://pubg.gamepedia.com/Severny", "images/maps/Severny.jpg"],
+	["Stalber", "https://pubg.gamepedia.com/Stalber", "images/maps/Stalber.jpg"],
+	["Zharki", "https://pubg.gamepedia.com/Zharki", "images/maps/Zharki.jpg"],
+	["Sosnovka Military Base", "https://pubg.gamepedia.com/Sosnovka_Military_Base", "images/maps/Military.jpg"]
 ];
+
 
 function RandomCity(){	
 	$(".a").hide();
